adec9711c91fdde8832391f4417cf93a7f8b8ffe
seungminJ <seungmin103@gmail.com>
2025-05-12 18:46:03 +0900

    first commit

---
+++ src/test/java/com/example/intern_assignment/InternAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerock.intern_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
